ce4f5d27c70cbdb59305fb0cd4e3dbe66d552c7f
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-11-23 15:52:52 -0500

    Convert relevant variables to factors

--- teamproj.R
+++ teamproj.R
@@ -10,6 +10,7 @@ data1 <- read_excel("transfusion data.xlsx")
 # Q2: In conjunction with the above, what is the impact of transfusion 
 # on patient outcomes, including mortality?
 
+# Subset the relevant columns
 data2_var <- c(
   # Demographics
   "STUDY ID #", "Gender (male)", "Age", "Height", "Weight", "BMI",
@@ -24,21 +25,26 @@ data2_var <- c(
   "Intra_Platelets", "Intra_Cryoprecipitate",
   
   # Mortality/Outcome variables
-  "ICU_LOS", "HOSPITAL_LOS", "DEATH_DATE",
+  "Duration of ICU Stay (days)", "Date of Extubation", "Duration of Ventilation",
+  "DEATH_DATE", "ALIVE_30DAYS_YN", "ALIVE_90DAYS_YN", "ALIVE_12MTHS_YN",
+  "ICU_LOS", "HOSPITAL_LOS",
   "Need for reoperation for bleeding within 24h"
 )
 
 data2 <- data[, data2_var]
 
-# Subset the relevant columns
+# Rename columns
 data2 <- data2 %>%
   rename(
+    # Demographics
     study_id = `STUDY ID #`,
     gender = `Gender (male)`,
     age = Age,
     height_cm = Height,
     weight_kg = Weight,
     bmi = BMI,
+    
+    # Transfusion variables
     total_rbc_24hr = `Total 24hr RBC`,
     massive_transfusion = `Massive Transfusion`,
     rbc_0_24 = `RBC 0-24hrs`,
@@ -62,14 +68,33 @@ data2 <- data2 %>%
     intra_pcc = `Intra_PCC/Octaplex`,
     intra_platelets = `Intra_Platelets`,
     intra_cryo = `Intra_Cryoprecipitate`,
+    
+    # Mortality/Outcome variables
+    icu_los_days = `Duration of ICU Stay (days)`,
+    extubation_date = `Date of Extubation`,
+    ventilation_duration = `Duration of Ventilation`,
+    death_date = DEATH_DATE,
+    alive_30d = ALIVE_30DAYS_YN,
+    alive_90d = ALIVE_90DAYS_YN,
+    alive_12m = ALIVE_12MTHS_YN,
     icu_los = ICU_LOS,
     hospital_los = HOSPITAL_LOS,
-    death_date = DEATH_DATE,
     reop_bleed_24hr = `Need for reoperation for bleeding within 24h`
   )
 
+
 str(data2)
+
 summary(data2)
 
+# Convert variables to factors
+data2 <- data2 %>%
+  mutate(
+    gender = factor(gender, levels = c(FALSE, TRUE), labels = c("No", "Yes")),
+    reop_24hr = factor(reop_bleed_24hr, levels = c(FALSE, TRUE), labels = c("No", "Yes")),
+    alive_30d = factor(alive_30d, levels = c("N", "Y"), labels = c("No", "Yes")),
+    alive_90d = factor(alive_90d, levels = c("N", "Y"), labels = c("No", "Yes")),
+    alive_12m = factor(alive_12m, levels = c("N", "Y"), labels = c("No", "Yes"))
+  )
 
 
