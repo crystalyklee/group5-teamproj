93a6d0c8ff77f02a8eed015adaa1b1746049276b
ibrahimemam7 <1ibrahimemam@gmail.com>
2024-11-25 16:02:10 -0500

    Began creating classification models

--- teamproj.R
+++ teamproj.R
@@ -3,6 +3,7 @@
 
 library(readxl)
 library(tidyverse)
+library(mice)
 
 ###############
 ## Data Prep ##
@@ -48,7 +49,118 @@ summary(d)
 d <- d %>% 
   mutate(transfusion = ifelse(`Total 24hr RBC` == 0, FALSE, TRUE))
 
-##########################
-## Logistic Lasso Model ##
-##########################
+# Impute missing data
+names(d) <- gsub("[^A-Za-z0-9]+", "_", names(d))
+imp <- mice(d, m = 1, seed = 7, print = F)
+d_imp <- complete(imp, 1)
+summary(d_imp)
 
+#################################
+## Lasso Model and Tree Models ##
+#################################
+
+# Create a version of the data without the RBC count because that is not a predictor
+d1 <- d_imp[,-47]
+
+# Create an empty dataframe to store the AUC for each model and respective replicates
+model.eval <- data.frame(model = NA, trial = NA, auc = NA)[0,]
+
+# Pick 5 seeds to randomize the testing/training split
+seeds <- sample(1:1000, 5)
+
+roc_plots <- list()
+
+# For each seed, create and evaluate the models (via AUC)
+for (i in 1:length(seeds)) {
+  
+  # Split data into training and test, equal size
+  set.seed(seeds[i])
+  train.I <- sample(nrow(d1),round(nrow(d1)/(10/7)))
+  
+  # Create model matrix, exclude intercept column
+  x <- model.matrix(transfusion ~.,d1)[train.I,-1]
+  
+  # Create a vector with the response values
+  y <- d1$transfusion[train.I]
+  
+  # Fit the model
+  lasso.mod <- glmnet(x,y,family="binomial")
+  
+  # Plot coefficient weights for different values of lambda
+  plot(lasso.mod,label = T, xvar = "lambda")
+  
+  # Use cross validation (k=5) to determine the optimal value of lambda
+  cv.lasso <- cv.glmnet(x,y,alpha=1,family = "binomial", type.measure = "auc", nfolds = 5)
+  plot(cv.lasso)
+  
+  # Extract the value of lambda
+  cv.lasso$lambda.min
+  
+  # Find the predictors in the model
+  coef(cv.lasso, s = "lambda.min")
+  
+  # Create predictions for the test set
+  pred.lasso <- as.numeric(predict(lasso.mod, newx = model.matrix(transfusion ~.,d1)[-train.I,-1], s=cv.lasso$lambda.min, type = "response"))
+  
+  # Plot the ROC curve
+  myroc <- roc(transfusion ~ pred.lasso, data=d1[-train.I,])
+  ggroc(myroc) + geom_abline(slope = 1, intercept = 1, linetype = "dashed", color = "red") + theme_classic() + ggtitle("ROC Curve with Diagonal Line")
+  plot(myroc, main = "Test")
+  
+  # Extracting the Area Under the Curve, a measure of discrimination
+  auc.lasso <- myroc$auc
+  auc.lasso
+  
+  # Add the auc to the dataframe
+  new_row <- data.frame(model = "Lasso", trial = i, auc = auc.lasso)
+  model.eval <- rbind(model.eval, new_row)
+  
+  # Create a classification tree
+  tree.mod <- tree(recurrence ~ . ,data = d3, subset=train.I)
+  plot(tree.mod)
+  text(tree.mod,pretty=0)
+  
+  
+  # Cross-validation for pruning
+  cv.res <- cv.tree(tree.mod, FUN=prune.tree)
+  cv.res
+  
+  best.size <- cv.res$size[which.min(cv.res$dev)]
+  best.size
+  
+  # Override size of 1 to avoid stump (as instructed)
+  best.size <- 2
+  
+  pruned <- prune.misclass(tree.mod,best=best.size)
+  
+  plot(pruned)
+  text(pruned,pretty=0)
+  
+  
+  # Calculating the accuracy on the test set for the pruned tree
+  preds.tmp <- predict(pruned,newdata=d3[-train.I,],type="vector")
+  pred.probs.tree <- as.numeric(preds.tmp[,2])
+  myroc <- roc(recurrence ~ pred.probs.tree, data=d3[-train.I,])
+  plot(myroc)
+  myroc$auc
+  
+  # Add the auc to the dataframe
+  new_row <- data.frame(model = "Pruned Tree", trial = i, auc = myroc$auc)
+  model.eval <- rbind(model.eval, new_row)
+  
+  # Calculate the accuracy on the test set for the unpruned tree
+  preds.tmp <- predict(tree.mod,newdata=d3[-train.I,],type="vector")
+  pred.probs.tree <- as.numeric(preds.tmp[,2])
+  myroc <- roc(recurrence ~ pred.probs.tree, data=d3[-train.I,])
+  plot(myroc)
+  myroc$auc
+  
+  # Add the auc to the dataframe
+  new_row <- data.frame(model = "Unpruned Tree", trial = i, auc = myroc$auc)
+  model.eval <- rbind(model.eval, new_row)
+}
+
+# Create a column graph to show the area under the curve for each model for each split
+ggplot(model.eval, aes(x = trial, y = auc, fill = model)) +
+  geom_bar(stat = "identity", position = position_dodge(), color = "black") +
+  theme_classic()
